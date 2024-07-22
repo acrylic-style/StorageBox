719b76417451b65f6f6c25f501584946a07c72e6
acrylic-style <me@acrylicstyle.xyz>
2024-07-22 12:35:42 +0900

    disable shulker box

--- src/main/java/xyz/acrylicstyle/storageBox/commands/ChangeTypeCommand.java
+++ src/main/java/xyz/acrylicstyle/storageBox/commands/ChangeTypeCommand.java
@@ -39,10 +39,10 @@ public class ChangeTypeCommand {
             player.sendMessage(ChatColor.RED + "オフハンドに変更先のアイテムを持ってからもう一度実行してください。");
             return;
         }
-//        if (offHand.getType().name().contains("SHULKER")) {
-//            player.sendMessage(ChatColor.RED + "このアイテムは格納できません。");
-//            return;
-//        }
+        if (offHand.getType().name().contains("SHULKER")) {
+            player.sendMessage(ChatColor.RED + "このアイテムは格納できません。");
+            return;
+        }
         ItemStack mainHand = player.getInventory().getItemInMainHand();
         StorageBox storageBox = StorageBox.getStorageBox(mainHand);
         if (storageBox == null) {
