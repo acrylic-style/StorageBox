2b297869412e7c32f3fbc0b708601260785c7b53
acrylic-style <me@acrylicstyle.xyz>
2024-11-04 16:30:22 +0900

    fix repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,9 +61,9 @@ publishing {
             credentials(PasswordCredentials::class)
             url = uri(
                 if (project.version.toString().endsWith("SNAPSHOT"))
-                    project.findProperty("deploySnapshotURL") ?: System.getProperty("deploySnapshotURL", "https://repo.acrylicstyle.net/repository/maven-snapshots/")
+                    project.findProperty("deploySnapshotURL") ?: System.getProperty("deploySnapshotURL", "https://repo.acrylicstyle.xyz/repository/maven-snapshots/")
                 else
-                    project.findProperty("deployReleasesURL") ?: System.getProperty("deployReleasesURL", "https://repo.acrylicstyle.net/repository/maven-releases/")
+                    project.findProperty("deployReleasesURL") ?: System.getProperty("deployReleasesURL", "https://repo.acrylicstyle.xyz/repository/maven-releases/")
             )
         }
     }
