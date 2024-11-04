b6c308551a7cd780b03f7de30c53599148582ef0
acrylic-style <me@acrylicstyle.xyz>
2024-11-04 16:27:19 +0900

    fix compile error

--- src/main/java/xyz/acrylicstyle/storageBox/network/ChannelUtil.java
+++ src/main/java/xyz/acrylicstyle/storageBox/network/ChannelUtil.java
@@ -14,7 +14,7 @@ public class ChannelUtil {
                     .connection
                     .channel
                     .pipeline()
-                    .addBefore("packet_handler", "azisaba_storagebox", new PacketListener(((CraftPlayer) player).getHandle()));
+                    .addBefore("packet_handler", "azisaba_storagebox", new PacketListener());
         } catch (Exception e) {
             Bukkit.getScheduler().runTask(plugin, () -> {
                 try {
@@ -24,7 +24,7 @@ public class ChannelUtil {
                             .connection
                             .channel
                             .pipeline()
-                            .addBefore("packet_handler", "azisaba_storagebox", new PacketListener(((CraftPlayer) player).getHandle()));
+                            .addBefore("packet_handler", "azisaba_storagebox", new PacketListener());
                 } catch (Exception e2) {
                     e2.addSuppressed(e);
                     e2.printStackTrace();
